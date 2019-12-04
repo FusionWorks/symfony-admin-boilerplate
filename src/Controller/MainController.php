d7a3a1567f8d6d978af5c96073d9fd7924ae55cc
Eynsteyn <eynsteyn@bk.ru>
2019-12-04 16:01:54 +0200

    Import Route

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -9,6 +9,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * Class MainController
--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("admin/products")
